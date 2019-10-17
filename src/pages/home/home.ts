c23e27f66f607e33186d9d3648a6342f190201b7
itzel alvarez <itzelalvarez56@gmail.com>
2019-10-16 20:26:47 -0500

    se crearon input y boton, se enlazaron a la funcion click y se creo la funcion alerta

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  nombre = '';
 
-  constructor(public navCtrl: NavController) {
 
+  constructor(public navCtrl: NavController,public alertCtrl: AlertController) {
+
+  }
+
+  click()
+  {
+    //console.log('Hola', this.nombre);
+    const alerta = this.alertCtrl.create({
+      title: 'Hola',
+      subTitle: this.nombre,
+      buttons: ['Ok', 'Cancel']
+    });
+
+    alerta.present();
   }
 
 }
